645ff57ba0d537a4af435213d10655d47f05e7c5
Nicolas Fernandez <nfernandez@evolvere.com.ar>
2026-06-08 14:48:39 -0300

    Cambios

--- resources/views/modules/operador/pesaje-detalle.blade.php
+++ resources/views/modules/operador/pesaje-detalle.blade.php
@@ -14,6 +14,10 @@
 
     {{-- Acciones --}}
     <div class="flex items-center gap-3 flex-1 justify-end">
+        <x-ui.button variant="outline" href="{{ $routeHistorial }}" class="w-full md:w-auto">
+            <x-lucide-history />
+            <span>Ver historial</span>
+        </x-ui.button>
         @unless($isAdmin)
             <x-ui.button href="{{ route('balanza') }}" class="w-full md:w-auto">
                 <x-lucide-plus />
